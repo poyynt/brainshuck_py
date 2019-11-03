d019ea280bd67435b8d9b1389f7a1c8aec0924e7
Parsa Torbati <ptorbatii@gmail.com>
2019-11-03 23:29:33 +0330

    better file structure, cleaned main code

---
+++ README.md
@@ -0,0 +1,16 @@
+# BrainShuck\_py
+
+BrainShuck\_py is a simple Brainf\*\*k interpreter written in Python.
+
+## Usage
+
+```
+python3 brainshuck.py file.bf
+```
+
+## Useful Resources
+
+[Learn Brainf\*\*k](https://esolangs.org/wiki/Brainfuck)
+
+
+
--- brainshuck.py
+++ brainshuck.py
@@ -1,23 +1,11 @@
 #/usr/bin/python3
 import sys
-
-def _mod(a, b):
-	return a%b if a>=0 else a if a>=-128 else a%b
-class int8(int):
-	def __add__(self, value):
-		self._val = _mod(value+self._val, 128)
-		return _mod(value+self.__int__(), 128) 
-	def __int__(self):
-		return _mod(self._val, 128)
-	def _chr(self):
-		return chr(self.__int__())
-
-
-
 try:
 	f = open(sys.argv[1])
 except:
-	sys.stderr.write("Bad arguement. Exiting...\n")
+	sys.stderr.write("Bad arguement.\n")
+	sys.stderr.write("Usage: \n")
+	sys.stderr.write(sys.argv[0] + " file.bf\n")
 	sys.exit()
 
 MEM = {0:0}
